58d09e485fd4f80d8fe330a8325aa8789e3fe184
tetiana_baranova <syniovych.tanya@gmail.com>
2025-03-20 19:36:32 +0200

    homepage

---
+++ src/pages/HomePage/HomePage.jsx
@@ -0,0 +1,12 @@
+import s from './HomePage.module.css';
+
+const HomePage = () => {
+    return (
+        <div>
+            <h1 className={s.title}>Welcome to Phonebook</h1>
+            <p className={s.text}>This application provides the ability to conveniently manage your
+        contacts and have access to your phone book.</p>
+        </div>
+    )
+}
+export default HomePage;
